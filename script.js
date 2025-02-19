97212a45b52126eac990f543f15bd09467deab71
Ryan Bush <51009458+ryan-bush1014@users.noreply.github.com>
2025-02-19 12:47:45 -0500

    Grid and translate

--- script.js
+++ script.js
@@ -13,10 +13,14 @@ document.addEventListener('resize', canvas.resize);
 
 class GridView {
     constructor(scale) {
-        this.centerX = 0; // In cell units
-        this.centerY = 0;
+        this.cornerX = 0; // In cell units
+        this.cornerY = 0;
         this.obstacles = []; // List of (x,y) cells representing obstacles
         this.scale = scale; // Size (in pixels) of one cell
+        this.numGridLinesX = Math.floor(canvas.width / this.scale);
+        this.numGridLinesY = Math.floor(canvas.width / this.scale);
+        this.offsetX = (this.cornerX * this.scale) % this.scale;
+        this.offsetY = (this.cornerY * this.scale) % this.scale;
     }
 
     addObstacle(x, y) {
@@ -24,15 +28,47 @@ class GridView {
     }
 
     translateView(dx, dy, cells = false) {
-        return null;
+        this.cornerX += dx / this.scale;
+        this.cornerY += dy / this.scale;
+        this.offsetX = (this.offsetX + dx) % this.scale;
+        this.offsetY = (this.offsetY + dy) % this.scale;
+        this.draw();
     }
 
-    scaleView(centerX, centerY, cells = false) {
+    scaleView(mouseX, mouseY, cells = false) {
         return null;
     }
 
     draw() {
-        return null;
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+        for(let i = 0; i < this.numGridLinesX + 2; ++i) {
+            ctx.fillRect((i - 0.005) * this.scale - this.offsetX, 0, this.scale * 0.01, canvas.height);
+        }
+        for(let j = 0; j < this.numGridLinesY + 2; ++j) {
+            ctx.fillRect(0, j * this.scale - this.offsetY, canvas.width, this.scale * 0.01);
+        }
     }
-
 }
+
+// Attach mouse movement to the translate function
+let mouseX = 0;
+let mouseY = 0;
+let drag = false;
+canvas.addEventListener('mousedown', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    drag = true;
+});
+canvas.addEventListener('mousemove', (e) => {
+    if(drag) {
+        gridView.translateView(mouseX - e.clientX, mouseY - e.clientY);
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }
+});
+canvas.addEventListener('mouseup', () => {drag = false;});
+
+let gridView = new GridView(100);
+gridView.draw();
+
+// ctx.fillRect(0,0,100,100);
